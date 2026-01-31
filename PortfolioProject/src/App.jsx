2f4fcf9ac058e38aec658130380679fba3a7f7ea
Kshitij Jha <connect.kshitijjha@gmail.com>
2026-01-31 23:17:07 +0530

    start

--- PortfolioProject/src/App.jsx
+++ PortfolioProject/src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
-import Hero from './Components/Hero/Hero'
-import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Hero/>
+
     </>
   )
 }
